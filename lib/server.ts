85f3c2a4f509f81cd343ecaca456412386ef01dd
felipecpassos <felipe.carvalho.passos@gmail.com>
2021-04-04 20:40:22 -0300

    more heroku config

--- lib/server.ts
+++ lib/server.ts
@@ -2,7 +2,7 @@ import app from "./config/app";
 import env from "./environment"
 
 const PORT = env.getPort();
-app.listen((process.env.PORT || PORT), () => {
+app.listen(process.env.PORT || PORT, () => {
    console.log('Express server listening on port ' + PORT);
 })
 
